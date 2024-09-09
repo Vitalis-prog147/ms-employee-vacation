aa99a9a3299d95abacc7cd50ceac99fd001e4929
Виталий Ляпин <v.lyapin@cn-innov.ru>
2024-09-09 16:45:59 +0300

    Initial commit

---
+++ src/main/java/org/example/MsEmployeeVacationApplication.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ms-employee-vacation",
+                description = "Микросервис по работе с отпусками сотрудников"
+        )
+)
+@SpringBootApplication
+public class MsEmployeeVacationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEmployeeVacationApplication.class, args);
+    }
+
+}
